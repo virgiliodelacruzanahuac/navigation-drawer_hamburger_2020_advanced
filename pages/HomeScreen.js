d7a59c6225559f38e20e837ca709e4a683cc2ece
virgiliodelacruzanahuac <70555842+virgiliodelacruzanahuac@users.noreply.github.com>
2020-09-06 18:56:40 -0500

    Add files via upload

---
+++ pages/HomeScreen.js
@@ -0,0 +1,44 @@
+
+import * as React from 'react';
+import { Button, View, Text, SafeAreaView } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1 , padding: 16}}>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Text
+            style={{
+              fontSize: 25,
+              textAlign: 'center',
+              marginBottom: 16
+            }}>
+            Home Screen
+          </Text>
+          <Button
+            onPress={() => navigation.navigate('SettingScreenStack')}
+            title="Go to Setting Screen"
+          />
+          <Button
+            onPress={() => navigation.navigate('ExploreScreen')}
+            title="Go to Explore Screen"
+          />
+        </View>
+        <Text style={{ fontSize: 18, textAlign: 'center', color: 'grey' }}>
+          Ejemplo React Navigate Drawer with Bottom Tab
+        </Text>
+        <Text
+          style={{ fontSize: 16, textAlign: 'center', color: 'grey' }}>
+          virgilio.delacruz@anahuac.mx
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default HomeScreen;
